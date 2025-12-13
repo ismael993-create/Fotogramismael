10af1f27fe642528413428998c7f7cb4feac8db7
ismael993-create <ismael.7@live.de>
2025-12-13 15:35:18 +0100

    java script und bilder hinzugefügt

---
+++ script.js
@@ -0,0 +1,23 @@
+let images = [
+  "img/photo1.svg",
+  "img/photo2.svg",
+  "img/photo3.svg",
+  "img/photo4.svg",
+  "img/photo5.svg",
+  "img/photo6.svg",
+  "img/photo7.svg",
+  "img/photo8.svg",
+  "img/photo9.svg",
+  "img/photo10.svg",
+  "img/photo11.svg",
+  "img/photo12.svg",
+];
+
+let gallery = document.getElementById("gallery");
+
+images.forEach((src) => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.classList.add("thumbnail");
+  gallery.appendChild(img);
+});
